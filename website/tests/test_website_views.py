787a1278146b242cc51a44794ca18139fc8be9e9
lchenri <lucasbonicardoso@outlook.com>
2024-07-11 01:35:25 -0300

    Mais testes implementados em views. Coverage quase em 100%

--- website/tests/test_website_views.py
+++ website/tests/test_website_views.py
@@ -2,9 +2,38 @@ from django.test import TestCase, Client
 from django.urls import reverse, resolve
 from .record_model_base import RecordModelBase
 from django.contrib.auth.models import User
-from ..forms import SignUpForm
+from ..forms import SignUpForm, AddRecordForm
 from website import views
 from django.contrib.messages import get_messages
+
+from ..models import Record
+
+
+def form_data_add_record():
+    form_data_add_record_form = {
+        'first_name': 'John',
+        'last_name': 'Doe',
+        'email': 'john@doe.com',
+        'phone': '3299990000',
+        'address': 'Rua dos Horizontes, 280',
+        'city': 'Felicidade das Rosas',
+        'state': 'Minas Gerais',
+        'zipcode': '33000200'
+    }
+    return form_data_add_record_form
+
+def form_data_add_record_edit():
+    form_data_add_record_form = {
+        'first_name': 'Pedro Henrique',
+        'last_name': 'Doe',
+        'email': 'john@doe.com',
+        'phone': '3299990000',
+        'address': 'Rua dos Horizontes, 280',
+        'city': 'Felicidade das Rosas',
+        'state': 'Minas Gerais',
+        'zipcode': '33000200'
+    }
+    return form_data_add_record_form
 
 
 class CRMViewsTest(TestCase, RecordModelBase):
@@ -122,10 +151,100 @@ class CRMViewsTest(TestCase, RecordModelBase):
         delete_record_view_url = resolve(reverse('crm:delete_record', kwargs={'pk': self.record_maker().id}))
         self.assertIs(delete_record_view_url.func, views.delete_record)
 
+    def test_delete_record_views_if_request_user_is_authenticated(self):
+        self.user = User.objects.create_user(username='testuser', password='208030Pl@')
+        self.record = self.record_maker()
+        self.client.login(username='testuser', password='208030Pl@')
+
+        response = self.client.post(reverse('crm:delete_record', kwargs={'pk': self.record.id}))
+
+        self.assertFalse(Record.objects.filter(pk=self.record.id).exists())
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].message, "Registro deletado com sucesso.")
+        self.assertRedirects(response, reverse('crm:home'))
+
+    def test_delete_record_views_if_request_user_is_not_authenticated(self):
+        self.record = self.record_maker()
+        response = self.client.post(reverse('crm:delete_record', kwargs={'pk': self.record.id}))
+
+        self.assertTrue(Record.objects.filter(pk=self.record.id).exists())
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].message, "Você deve estar logado para realizar essa ação.")
+        self.assertRedirects(response, reverse('crm:home'))
+
     def test_add_record_records_views_function_is_correct(self):
         add_record_view_url = resolve(reverse('crm:add_record'))
         self.assertIs(add_record_view_url.func, views.add_record)
 
+    def test_add_record_views_if_request_user_is_authenticated(self):
+        self.user = User.objects.create_user(username='testuser', password='203060Pl@')
+        self.client.login(username='testuser', password='203060Pl@')
+
+        response = self.client.post(reverse('crm:add_record'), form_data_add_record())
+
+        self.assertTrue(Record.objects.filter(email='john@doe.com').exists())
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].message, "Registro salvo com sucesso.")
+        self.assertRedirects(response, reverse('crm:home'))
+
+    def test_add_record_views_if_request_user_is_not_authenticated(self):
+        response = self.client.post(reverse('crm:add_record'), form_data_add_record())
+        self.assertFalse(Record.objects.filter(email=form_data_add_record().get('email')).exists())
+
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].message, "Você deve estar logado para realizar essa ação.")
+        self.assertRedirects(response, reverse('crm:home'))
+
+    def test_add_record_views_if_request_user_is_authenticated_and_request_method_is_not_post(self):
+        self.user = User.objects.create_user(username='testuser', password='203060Pl@')
+        self.client.login(username='testuser', password='203060Pl@')
+
+        response = self.client.get(reverse('crm:add_record'), form_data_add_record())
+
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 0)
+        self.assertNotContains(response, "Registro salvo com sucesso.")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'website/pages/add_record.html')
+        self.assertIsInstance(response.context['form'], AddRecordForm)
+
+    def test_add_record_views_if_form_is_not_valid(self):
+        self.user = User.objects.create_user(username='testuser', password='203060Pl@')
+        self.client.login(username='testuser', password='203060Pl@')
+
+        invalid_form = {key: '' for key in form_data_add_record()}
+        response = self.client.post(reverse('crm:add_record'), invalid_form)
+
+        self.assertFalse(Record.objects.filter(email='john@doe.com').exists())
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 0)
+        self.assertIsInstance(response.context['form'], AddRecordForm)
+        self.assertTrue(response.context['form'].errors)
+
     def test_update_record_records_views_function_is_correct(self):
         update_record_view_url = resolve(reverse('crm:update_record', kwargs={'pk': self.record_maker().id}))
         self.assertIs(update_record_view_url.func, views.update_record)
+
+    def test_update_record_views_if_request_user_is_authenticated(self):
+        self.user = User.objects.create_user(username='testuser', password='203060Pl@')
+        self.client.login(username='testuser', password='203060Pl@')
+        self.record = self.record_maker()
+        form_edit = form_data_add_record_edit()
+
+        response = self.client.post(reverse('crm:update_record', kwargs={'pk': self.record.id}), form_edit)
+
+        self.record.refresh_from_db()
+        self.assertEqual(self.record.first_name, form_edit.get('first_name'))
+
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].message, "Registro editado com sucesso.")
+        self.assertRedirects(response, reverse('crm:home'))
+
+
+
+
